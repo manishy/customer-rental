86a5f0374003547c18f6912fe276bdb52ec8e705
Manish <manishyadav12354@gmail.com>
2020-08-01 23:45:23 +0530

    Extract amount, isNewRelease, isBonusApplied and frequentRenterPoints to Rental class

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -2,19 +2,55 @@ package com.thoughtworks.movierental;
 
 public class Rental {
 
-  private int daysRented;
-  private Movie movie;
+    private int daysRented;
+    private Movie movie;
 
-  public Rental(Movie movie, int daysRented){
-    this.movie = movie;
-    this.daysRented = daysRented;
-  }
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
 
-  public int getDaysRented() {
-    return daysRented;
-  }
+    public int getDaysRented() {
+        return daysRented;
+    }
 
-  public Movie getMovie() {
-    return movie;
-  }
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public double amount() {
+        double amount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (getDaysRented() > 2)
+                    amount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (getDaysRented() > 3)
+                    amount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    public boolean isNewRelease() {
+        return getMovie().getPriceCode() == Movie.NEW_RELEASE;
+    }
+
+    public boolean isBonusApplied() {
+        return isNewRelease()
+                &&
+                getDaysRented() > 1;
+    }
+
+    public int frequentRenterPoints() {
+        // add bonus for a two day new release rental
+        if (isBonusApplied()) return  2;
+        return 1;
+    }
 }
